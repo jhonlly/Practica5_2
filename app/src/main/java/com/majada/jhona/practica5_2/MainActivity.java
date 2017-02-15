fb4852605002e3fa1e37625e1c1100f0fd5638c8
Jhon <jhonlly_blim_blim@hotmail.com>
2017-02-15 20:00:19 +0000

    Tarea finalizada.

---
+++ app/src/main/java/com/majada/jhona/practica5_2/MainActivity.java
@@ -0,0 +1,82 @@
+package com.majada.jhona.practica5_2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LocationManager gestorLoc = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        gestorLoc.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String text = "Posición actual:\n" +
+                "Latitud = " + location.getLatitude() + "\n" +
+                "Longitud = " + location.getLongitude();
+      //  Toast.makeText(getApplicationContext(), text,Toast.LENGTH_LONG).show();
+        tv1.setText(text);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        String missatge = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                missatge = "GPS status: Out of service";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                missatge = "GPS status: Temporarily unavailable";
+                break;
+            case LocationProvider.AVAILABLE:
+                missatge = "GPS status: Available";
+                break;
+        }
+        Toast.makeText(getApplicationContext(),
+                missatge, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+        Toast.makeText(getApplicationContext(),
+                "GPS activado por el usuario",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(getApplicationContext(),
+                "GPS desactivado por el usuario",
+                Toast.LENGTH_LONG).show();
+    }
+}
